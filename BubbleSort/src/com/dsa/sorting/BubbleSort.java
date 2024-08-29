63cd89fa59c899b85cd5c885901dd59aae4f4946
Munsi Md Iftabuddin <munsi.iftabuddin@saviynt.com>
2024-08-29 19:17:08 +0530

    Correct Name

--- BubbleSort/src/com/dsa/sorting/BubbleSort.java
+++ BubbleSort/src/com/dsa/sorting/BubbleSort.java
@@ -7,14 +7,14 @@ public class BubbleSort {
 	 * Takes O(n^2)
 	 * */
 	
-	public static void swap(int[] arr, int startIndex, int endIndex) {
-		int temp = arr[startIndex];
-		arr[startIndex] = arr[endIndex];
-		arr[endIndex] = temp;
+	public static void swap(int[] arr, int index_1, int index_2) {
+		int temp = arr[index_1];
+		arr[index_1] = arr[index_2];
+		arr[index_2] = temp;
 	}
 
 	public static void main(String[] args) {
-		int[] input = {1,32,4,2,1,223,43,9};
+		int[] input = {1, 32, 4, 2, 1, 223, 43, 9};
 		
 		for(int i = input.length -1 ; i > 0; i--) {
 			for(int j = 0 ; j < i; j++) {
@@ -23,7 +23,7 @@ public class BubbleSort {
 				}
 			}
 		}
-		
+		// Printing Output
 		Arrays.stream(input).forEach(s -> System.out.print(s+"\t"));
 	}
 
